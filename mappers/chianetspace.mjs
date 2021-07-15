bd911fe9ba2aa54c34cdbe174d1e868be46dc719
horat1us <reclamme@gmail.com>
2021-07-15 17:49:01 +0300

    Refactor code

--- mappers/chianetspace.mjs
+++ mappers/chianetspace.mjs
@@ -4,7 +4,8 @@ const parseSymbol = (symbol) => {
   return [first, "i", last].join("").toLowerCase();
 };
 
-const chianetspaceMapper = (source) => {
+// parsing https://chianetspace.azurewebsites.net/data/summary
+export const chianetspace = (source) => {
   const { netSpace, chiaPrice, lastUpdateDate } = source;
 
   return {
@@ -14,8 +15,9 @@ const chianetspaceMapper = (source) => {
       value: netSpace.largestWholeNumberDecimalValue,
       unit: parseSymbol(netSpace.largestWholeNumberDecimalSymbol),
     },
+    // @link https://www.chia.net/assets/Chia-Business-Whitepaper-2021-02-09-v1.0.pdf page 9
     chances: 4608,
   };
 };
 
-export default chianetspaceMapper;
+export default chianetspace;
---
+++ mappers/index.mjs
@@ -0,0 +1,1 @@
+export * from "./chianetspace.mjs";
